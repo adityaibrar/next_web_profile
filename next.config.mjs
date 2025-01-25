1662956e42da56dd1767797371deb2e9beb97f00
aditya.ibrar <98072529+adityaibrar@users.noreply.github.com>
2025-01-25 11:56:51 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
 import mdx from "@next/mdx";
 
-const withMDX = mdx({
-  extension: /\.mdx?$/,
-  options: {},
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   output: "export"
 };
 
-export default withMDX(nextConfig);
+export default mdx()(nextConfig);
